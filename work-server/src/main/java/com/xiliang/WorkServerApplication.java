e9bf215e933ce40640898bdf03ec999f5c700a33
xiliang <xiliang2132@gmail>
2025-09-03 13:07:24 +0800

    仓库管理系统项目基础框架

---
+++ work-server/src/main/java/com/xiliang/WorkServerApplication.java
@@ -0,0 +1,13 @@
+package com.xiliang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkServerApplication.class, args);
+    }
+
+}
---
+++ work-server/src/test/java/com/xiliang/WorkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiliang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
